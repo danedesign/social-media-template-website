26ef61e24b9e9e3862e467dd4a2ca55f14d6bfef
dane.design <daienlyu@foxmail.com>
2023-01-15 17:19:49 +1100

    Initial Release

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-
+$(document).ready(function () {
+    $('.header-profile-dropmenu').hide();
+    $('.header-profile-img').click(function () {
+        $('.header-profile-dropmenu').fadeToggle("fast");
+    })
+})
